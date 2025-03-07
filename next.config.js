8aaa947b5f90a08e6a67f768b7dc5c5bda974f13
Isham Khan <ishamkhan@Ishams-MacBook-Air-2.local>
2025-03-06 21:39:58 -0500

    Fix Next.js image loading on Vercel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   trailingSlash: false,
+  images: {
+    unoptimized: true, // Fixes images not loading in production
+  },
 };
 
 module.exports = nextConfig;
